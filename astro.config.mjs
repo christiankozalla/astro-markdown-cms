5eb7edcef56d08298eb709d96adf5caaaebbffbb
Christian Kozalla <christian.kozalla@gmail.com>
2022-09-13 20:45:39 +0200

    chore: remove sitemap npm package because it only supports static builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap()],
   output: 'server',
   adapter: node()
 });
